4b6f20cc5f1cca04e7b9ec7bffc52dbc7f1cc115
N3f4st <jurg_92@hotmail.com>
2022-05-20 19:02:51 -0600

    validation for not handled paths

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ app.use('/test', (req, resp, done)=> {
     return resp.json('Todo genial, ¿cierto?').status(200);
 })
 
+// For not hanlded paths
+app.use((req, res, done)=> {
+    let err = new Error('Invalid requested path :(');
+    err.status = 404;
+    done(err);
+});
+
 let server = app.listen((process.env.PORT || 5000), () => {
     console.log(`Listening dogs server on port ${(process.env.PORT || 5000)}`);
 });
